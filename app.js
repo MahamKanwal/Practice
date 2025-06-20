dba14cfcb7104a44d1aff9cbf8c02247fad7b3d0
Maham Kanwal <kanwalmahsm@gmail.com>
2025-06-20 16:16:37 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,105 @@
+// alert("Thanks for your input");
+
+// var name = "Maham"; 
+// name = "Kanwal";
+// alert(name);
+
+// var nationality;
+// nationality = "Pakistani";
+// console.log(nationality);
+
+// var thanx = "Thannks for your input";
+// alert("thanx");
+
+// var weight = 150;
+// console.log(weight);
+
+// var originNum = 23;
+// var newNum = originNum + 7;
+// alert(newNum);
+
+// var originNum = 28;
+// var numToBeAdded = 7;
+// var newNum = originNum + numToBeAdded;
+// alert(newNum);
+
+// var originNum = 90;
+// originNum = originNum + 50;
+// alert(originNum);
+
+// var originNum = "23";
+// var newNum = originNum + 7;
+// console.log(newNum);
+
+// var caseOty = 144;
+// alert(caseOty);
+
+// var popularNum =  3 + 12;
+// alert(popularNum)
+
+// var whatsLeftOver = 10 % 3;
+// alert(whatsLeftOver);
+
+// var num = 10;
+// var popularNumber = num + 50;
+// console.log(popularNumber);
+
+// var num = 50;
+// var anotherNum = 7; 
+// var popularNumber = num + anotherNum;
+// console.log(popularNumber);
+
+//  Increment (اضافہ کرنا)
+// **Increment ka matlab hota hai kisi number ya variable ke value ko 1 se barhāna.
+
+// javascript
+// Copy
+// Edit
+// let x = 5;
+// x++; // increment
+// console.log(x); Output: 6
+// x++ ka matlab hai x = x + 1
+
+// 👉 Types of Increment:
+
+// Post-increment: x++
+// (Pehle value use hoti hai, phir barhti hai)
+
+// Pre-increment: ++x
+// (Pehle value barhti hai, phir use hoti hai)
+
+// javascript
+// Copy
+// Edit
+// let x = 5;
+// console.log(x++);  Output: 5 (pehle print phir barhaya)
+// console.log(x);   Output: 6
+
+// let y = 5;
+// console.log(++y); Output: 6 (pehle barhaya phir print)
+// 🔹 Decrement (کم کرنا)
+// **Decrement ka matlab hota hai kisi number ya variable ki value ko 1 se ghatāna.
+
+// javascript
+// Copy
+// Edit
+// let a = 5;
+// a--; decrement
+// console.log(a);  Output: 4
+// a-- ka matlab hai a = a - 1
+
+// 👉 Types of Decrement:
+
+// Post-decrement: a--
+
+// Pre-decrement: --a
+
+// javascript
+// Copy
+// Edit
+// let a = 5;
+// console.log(a--);  Output: 5 (pehle print phir kam kiya)
+// console.log(a);    Output: 4
+
+// let b = 5;
+// console.log(--b);  Output: 4 (pehle kam kiya phir print)
